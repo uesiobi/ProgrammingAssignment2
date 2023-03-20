69bc116b259a6d157bb29e2bfff2cc2b6a2cdba6
uesiobi <uzo2ec@gmail.com>
2023-03-20 18:55:10 +0100

    Two functions were created, one is to create a natrix (makeCacheMatrix) while the seconds returns the inverse of the matrix (cacheSolve).

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This function is used for Caching the Inverse of a Matrix
 
-## Write a short comment describing this function
+## This function is a special matrix that is invertible
 
 makeCacheMatrix <- function(x = matrix()) {
+    z <- NULL
+    set <- function(y) {
+        x <<- y
+        z <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(inverse) z <<- inverse
+    getinverse <- function() z
+    list(set = set,
+         get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
+}  
 
-}
-
-
-## Write a short comment describing this function
+## This function returns the inverse of the main matrix as the output.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    z <- x$getinverse()
+    if (!is.null(z)) {
+        message("getting cached data")
+        return(z)
+    }
+    data <- x$get()
+    z <- solve(data, ...)
+    x$setinverse(z)
+    z
+        ##  z Returns a matrix that is the inverse of 'x'
 }
